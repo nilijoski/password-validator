b0b11549ad99da619c57943a21b3daef7f85acb6
Niko Ilijoski <n_ilijoski@outlook.de>
2025-10-14 23:38:06 +0200

    REFACTOR: use @BeforeEach to initialize the utility class once for every test case

--- src/test/java/com/example/PasswordValidatorTest.java
+++ src/test/java/com/example/PasswordValidatorTest.java
@@ -1,66 +1,65 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PasswordValidatorTest {
 
+    private PasswordValidator passwordValidator;
+
+    @BeforeEach
+    void setUp() {
+        passwordValidator = new PasswordValidator();
+    }
+
     @Test
     void hasMinLength_ShouldReturnFalse_WhenLength7() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength("1234567", 8));
+        assertFalse(passwordValidator.hasMinLength("1234567", 8));
     }
 
     @Test
     void hasMinLength_ShouldReturnTrue_WhenLength8() {
-        PasswordValidator pv = new PasswordValidator();
-        assertTrue(pv.hasMinLength("12345678", 8));
+        assertTrue(passwordValidator.hasMinLength("12345678", 8));
     }
 
     @Test
     void hasMinLength_ShouldReturnTrue_WhenLength10() {
-        PasswordValidator pv = new PasswordValidator();
-        assertTrue(pv.hasMinLength("1234567890", 8));
+        assertTrue(passwordValidator.hasMinLength("1234567890", 8));
     }
 
     @Test
     void hasMinLength_ShouldReturnFalse_WhenGivenNull() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength(null, 8));
+        assertFalse(passwordValidator.hasMinLength(null, 8));
     }
 
 
     @Test
     void hasMinLength_ShouldReturnFalse_WhenGivenBlankSpacesExist() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength(" 123 ", 5));
+        assertFalse(passwordValidator.hasMinLength(" 123 ", 5));
     }
 
     @Test
     void hasMinLength_ShouldReturnFalse_WhenLength0() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength("", 8));
+        assertFalse(passwordValidator.hasMinLength("", 8));
     }
 
     @Test
     void hasMinLength_ShouldReturnFalse_WhenMinIsZero() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength("abc", 0));
+        assertFalse(passwordValidator.hasMinLength("abc", 0));
     }
 
     @Test
     void hasMinLength_ShouldReturnFalse_WhenMinIsNegative() {
-        PasswordValidator pv = new PasswordValidator();
-        assertFalse(pv.hasMinLength("abc", -1));
+        assertFalse(passwordValidator.hasMinLength("abc", -1));
     }
 
     @Test
     void hasMinLength_ShouldReturnFalse_ForVeryLongPassword() {
-        PasswordValidator pv = new PasswordValidator();
         String longPassword = "a".repeat(1000);
-        assertFalse(pv.hasMinLength(longPassword, 8));
+        assertFalse(passwordValidator.hasMinLength(longPassword, 8));
     }
-
 
 }
